21ece6a03784d655817ed06a0c4f137958589be5
echonanguo <zhuyuqinss@gmail.com>
2025-05-31 22:22:14 +0800

    monitor

---
+++ lemall-monitor/src/main/java/org/echonanguo/lemall/LemallMonitorApplication.java
@@ -0,0 +1,16 @@
+package org.echonanguo.lemall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: echonanguo
+ * @CreateTime: 2025-05-31
+ * @Description: lemall监控微服务启动类
+ */
+@SpringBootApplication
+public class LemallMonitorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LemallMonitorApplication.class, args);
+    }
+}
