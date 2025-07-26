a3260f153541584885127cfbfbea418d76ada9b3
zhuyuqin <echonanguo@qq.com>
2025-07-26 22:04:06 +0800

    项目初始化

---
+++ lemall-server/lemall-business/lemall-module-admin/src/main/java/org/nanguo/lemall/test/TestController.java
@@ -0,0 +1,12 @@
+package org.nanguo.lemall.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("hello")
+    public String hello() {
+        return "hello";
+    }
+}
