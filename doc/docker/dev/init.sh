362213c8f168a5e10ed1d8067c611c6fd2abe6cb
zhuyuqin <echonanguo@qq.com>
2025-08-17 19:44:46 +0800

    各种配置和中间件

--- doc/docker/dev/init.sh
+++ doc/docker/dev/init.sh
@@ -3,29 +3,8 @@
 mkdir -p ./mysql/data
 mkdir -p ./mysql/conf
 
-mkdir -p ./rabbitmq/data
-
 mkdir -p ./redis/data
-
-mkdir -p ./mongodb-data
-
-mkdir -p ./nacos/logs
-mkdir -p ./nacos/data
-
-mkdir -p ./logstash/config
-
-# 导入logstash配置
-curl -o ./logstash/config/logstash.conf https://raw.githubusercontent.com/echonanguo/lemall/refs/heads/master/doc/conf/logstash.conf
-
-mkdir -p ./elasticsearch/data
-mkdir -p ./elasticsearch/plugins
-mkdir -p ./elasticsearch/logs
-
-# 下载ik分词器
-curl -o ./ik.zip https://release.infinilabs.com/analysis-ik/stable/elasticsearch-analysis-ik-8.10.4.zip
-mkdir ./elasticsearch/plugins/ik
-unzip ./ik.zip -d ./elasticsearch/plugins/ik
-rm ./ik.zip
 
 mkdir -p ./minio/data
 echo "文件已创建"
+
